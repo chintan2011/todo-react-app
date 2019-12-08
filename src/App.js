8b65ff9c8c316f216d572a4770d5c4d122656d0d
Chintan Savalia <chintan2011@gmail.com>
2019-12-08 13:50:20 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import './App.css';
+import Todos from './components/Todos';
+
+class App extends Component {
+
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Breakfast',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Check Weather Report',
+        completed: true 
+      }
+    ]
+  }
+
+  // Toggle Complete timing 51:15
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id == id){
+        todo.completed = !todo.completed
+      }
+      return todo;
+    })  });
+  }
+  render (){
+    console.log(this.state.todos);
+    return (
+      <div className="App">
+      <Todos  todos={this.state.todos} 
+              markComplete={this.markComplete}  />
+      </div>
+    )
+  }
+}
+
+export default App;
